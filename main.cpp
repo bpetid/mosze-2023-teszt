5c01345a9a6c24b64759006c2b689108e4e78a4b
Bányai Péter <bpeitd@pm.me>
2023-09-18 17:07:53 +0200

    Fix 1-100 bug

--- main.cpp
+++ main.cpp
@@ -2,17 +2,12 @@
 
 constexpr int N_ELEMENTS = 100;
 
-/*
- *  mivel az N_ELEMENTS értéke 100, a fn-ek pedig 0-100-ig, kizárólag működnek,
- *  így valójában nem 1-100-ig duplázza az értékeket, hanem 0-99-ig.
- */
-
 int main() {
     int *b = new int[N_ELEMENTS];
 
     std::cout << "1-100 ertekek duplazasa" << std::endl;
     for (int i = 0; i < N_ELEMENTS; i++) { 
-        b[i] = i * 2; // <~~~~~ a hibát itt fogom javítani
+        b[i] = (i + 1) * 2;
     }
 
     for (int i = 0; i < N_ELEMENTS; i++) {
