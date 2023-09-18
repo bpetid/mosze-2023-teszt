fef1451bd9a46c58cf9b9ddf8a2facd69235abf7
Bányai Péter <bpeitd@pm.me>
2023-09-18 17:00:52 +0200

    Fix basic bugs in code

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 constexpr int N_ELEMENTS = 100;
 
 int main() {
-    int *b = new int[NELEMENTS]; // N_ELEMENTS helytelen
+    int *b = new int[N_ELEMENTS];
 
-    std::cout << '1-100 ertekek duplazasa' // ; hiányzik, "-" helytelen
-    for (int i = 0;) { // nincs befejezve a feltétel
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++) { 
         b[i] = i * 2;
     }
 
-    for (int i = 0; i; i++) {   // range nincs megadva
-        std::cout << "Ertek:"   // ; és érték hiányzik
+    for (int i = 0; i < N_ELEMENTS; i++) {
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
 
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) { // , szerepel ; helyett
-        atlag += b[i] // ; hiányzik
+    for (int i = 0; i < N_ELEMENTS; i++) {
+        atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
