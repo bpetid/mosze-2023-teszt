e753872f5fc3d04aeb7d30aa015116047f6c972e
Bányai Péter <bpeitd@pm.me>
2023-09-18 16:56:18 +0200

    Comment on bugs inside main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,24 @@
 
 constexpr int N_ELEMENTS = 100;
 
-int main()
-{
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
-    {
+int main() {
+    int *b = new int[NELEMENTS]; // N_ELEMENTS helytelen
+
+    std::cout << '1-100 ertekek duplazasa' // ; hiányzik, "-" helytelen
+    for (int i = 0;) { // nincs befejezve a feltétel
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
+
+    for (int i = 0; i; i++) {   // range nincs megadva
+        std::cout << "Ertek:"   // ; és érték hiányzik
     }    
+
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
+    for (int i = 0; i < N_ELEMENTS, i++) { // , szerepel ; helyett
+        atlag += b[i] // ; hiányzik
     }
+
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
